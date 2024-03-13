58ff378acc717ffea4b6c2a188611bdb7990edbd
Richard Schepen <9021227@student.zadkine.nl>
2024-03-13 11:11:25 +0100

    LIkecounter werkend gemaakt
    Nu kan je eindelijk je mening uiten op posts!

--- JS/LikeTweet.js
+++ JS/LikeTweet.js
@@ -1,4 +1,10 @@
-function likeTweet(x) {
-    x.classList.toggle("fa-thumbs-down");
-  }
-   
+var likeButton = document.getElementById("likeButton");
+var likeCount = 0;
+
+function likeTweet() {
+  likeButton.classList.toggle("liked");
+  likeCount += likeButton.classList.contains("liked") ? 1 : -1;
+  
+  var likeCounterElement = document.querySelector(".like-counter");
+  likeCounterElement.textContent = likeCount;
+}
