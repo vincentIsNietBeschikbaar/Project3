72a83e44170c801042893948be5ce0938ed1d303
Vincent <buntvincent@gmail.comm>
2024-03-15 10:24:46 +0100

    twee recente versies gemerged
    Richard's HTML code staat nu in hoofdpagina.php

--- PHP/hoofdpagina.php
+++ PHP/hoofdpagina.php
@@ -9,36 +9,33 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../CSS/style.css">
 </head>
-
+ 
 <body>
-
+    <div>
+        <i id="heart" class="fa fa-heart"></i>
+    </div>
     <nav>
         <p class="homeBar">Home</p>
     </nav>
-
+ 
     <nav class="actions">
         <button class="makeChirpButton" id="makeChirpButton">Maak een Chirp</button>
     </nav>
 
 
-
-
-    <nav id="cloneTweet" class="tweet">
-        <i onclick="likeTweet(this)" class="fa fa-thumbs-up"></i>
-        <p id="likeButton" class="like-button">
-            <span class="like-counter">0</span>
+    <nav id="cloneTweet" class="tweet" data-post-id="1">
+        <i onclick="likeTweet(this)" class="fa fa-heart"></i>
+        <p class="like-button">
+            <span class="likeCounter">0</span>
         </p>
-
         <nav class="profileBar">
             <p class="naamInTweet">Gebruiker</p>
         </nav>
-
         <nav class="tweetBox">
-
-            <p class="textInTweet">Al is een 🐎 nog zo snel, een frikandel word het toch wel!</p>
+            <p class="textInTweet">Plaats uw tekst hier</p>
         </nav>
     </nav>
 
